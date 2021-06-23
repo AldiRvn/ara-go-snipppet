aaf418ea30c67e0a006908b27b44d71820c48715
Arawan <45452761+AldiRvn@users.noreply.github.com>
2021-06-23 21:54:32 +0700

    Update http.go

--- http.go
+++ http.go
@@ -1,3 +1,37 @@
+type HTTP struct{}
+
+func (this HTTP) bodyBuilder(requestBody interface{}) io.Reader {
+	requestBodyAsByte, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil
+	}
+
+	return bytes.NewBuffer(requestBodyAsByte)
+}
+func (this HTTP) HTTPPost(url string, requestBody interface{}, addHeader map[string]interface{}) ([]byte, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest("POST", url, this.bodyBuilder(requestBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = http.Header{
+		"Content-Type": []string{"application/json"},
+	}
+	for key, value := range addHeader {
+		req.Header.Set(key, fmt.Sprint(value))
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(res.Body)
+}
+
 // Godoc HTTPPostJSON
 // ref: https://blog.logrocket.com/making-http-requests-in-go/
 func HTTPPostJSON(url string, postBody interface{}) (string, error) {
