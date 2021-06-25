7fdf9366a831be2b2b69273061f6442a94ba0a6b
Arawan <45452761+AldiRvn@users.noreply.github.com>
2021-06-25 18:55:57 +0700

    Update http.go
    fix httpUtility.GET error because body is nill instead {}

--- http.go
+++ http.go
@@ -36,7 +36,7 @@ func (this HTTP) POST(url string, requestBody interface{}, addHeader map[string]
 	return this.httpRequest("POST", url, requestBody, addHeader)
 }
 func (this HTTP) GET(url string, addHeader map[string]interface{}) ([]byte, error) {
-	return this.httpRequest("GET", url, nil, addHeader)
+	return this.httpRequest("GET", url, map[string]interface{}{}, addHeader)
 }
 
 // Godoc HTTPPostJSON
